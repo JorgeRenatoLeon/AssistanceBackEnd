413457293a77f86554a3014d8ec2c171435db766
Jorge Leon <jrleon@pucp.edu.pe>
2020-08-02 18:19:12 -0500

    Login Decrypt

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -362,10 +362,46 @@ class UsuarioController extends Controller
         return response()->json($pila);
     }
 
+    public function encrypt($value, string $passphrase)
+    {
+        $salt = openssl_random_pseudo_bytes(8);
+        $salted = '';
+        $dx = '';
+        while (strlen($salted) < 48) {
+            $dx = md5($dx . $passphrase . $salt, true);
+            $salted .= $dx;
+        }
+        $key = substr($salted, 0, 32);
+        $iv = substr($salted, 32, 16);
+        $encrypted_data = openssl_encrypt(json_encode($value), 'aes-256-cbc', $key, true, $iv);
+        $data = ["ct" => base64_encode($encrypted_data), "iv" => bin2hex($iv), "s" => bin2hex($salt)];
+        return json_encode($data);
+    }
+
+
+    public function decrypt(string $jsonStr, string $passphrase)
+    {
+        $json = json_decode($jsonStr, true);
+        $salt = hex2bin($json["s"]);
+        $iv = hex2bin($json["iv"]);
+        $ct = base64_decode($json["ct"]);
+        $concatedPassphrase = $passphrase . $salt;
+        $md5 = [];
+        $md5[0] = md5($concatedPassphrase, true);
+        $result = $md5[0];
+        for ($i = 1; $i < 3; $i++) {
+            $md5[$i] = md5($md5[$i - 1] . $concatedPassphrase, true);
+            $result .= $md5[$i];
+        }
+        $key = substr($result, 0, 32);
+        $data = openssl_decrypt($ct, 'aes-256-cbc', $key, true, $iv);
+        return json_decode($data, true);
+    }
+
     public function vuelogin(Request $request)
     {
-
-        if(Auth::attempt(['correo' => $request->correo, 'password' => $request->password])){
+        $pass = $this->decrypt($request->password);
+        if(Auth::attempt(['correo' => $request->correo, 'password' => $pass])){
             $finduser = Usuario::where('correo', $request->correo)->first();
             if($finduser->estado == 'act'){
                 Auth::loginUsingId($finduser->id_usuario);
@@ -434,8 +470,7 @@ class UsuarioController extends Controller
                 }
                 else{
                     return response()->json([
-                        'status' => 'error',
-                        'user'   => 'Unauthorized Access'
+                        'status' => 'Cuenta Inhabilitada',
                     ]);
                 }
             } else {
@@ -443,7 +478,7 @@ class UsuarioController extends Controller
             }
         }
         else{
-            return response()->json(['status'=> 'No es un correo de la institucion']);
+            return response()->json(['status'=> 'No es un correo de la institución']);
         }
     }
 
