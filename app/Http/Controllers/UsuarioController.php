4282ca3b6026708f1243cfd6bb187e97ae22b202
Jorge Leon <jrleon@pucp.edu.pe>
2020-08-02 20:18:00 -0500

    Decriptar Password

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -362,9 +362,30 @@ class UsuarioController extends Controller
         return response()->json($pila);
     }
 
+    public function CryptoJSAesDecrypt($passphrase, $jsonString){
+
+
+        $jsondata = json_decode($jsonString, true);
+        $salt = hex2bin($jsondata["s"]);
+        $ct = base64_decode($jsondata["ct"]);
+        $iv  = hex2bin($jsondata["iv"]);
+        $concatedPassphrase = $passphrase.$salt;
+        $md5 = array();
+        $md5[0] = md5($concatedPassphrase, true);
+        $result = $md5[0];
+        for ($i = 1; $i < 3; $i++) {
+            $md5[$i] = md5($md5[$i - 1].$concatedPassphrase, true);
+            $result .= $md5[$i];
+        }
+        $key = substr($result, 0, 32);
+        $data = openssl_decrypt($ct, 'aes-256-cbc', $key, true, $iv);
+        return json_decode($data, true);
+
+    }
+
     public function vuelogin(Request $request)
     {
-        $pass = openssl_decrypt($request->password,"AES-256-ECB",'assistancesoporte');
+        $pass = $this->CryptoJSAesDecrypt("assistancesoporte",$request->password);
         if(Auth::attempt(['correo' => $request->correo, 'password' => $pass])){
             $finduser = Usuario::where('correo', $request->correo)->first();
             if($finduser->estado == 'act'){
